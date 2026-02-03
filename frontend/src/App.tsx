e8c989a44e0f61a873b2b77b7f245196de91e1f2
vijay18vp <voidprotocol18@gmail.com>
2026-02-03 12:26:11 +0530

    Vercel deplo analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PomodoroProvider } from "@/contexts/PomodoroContext";
+import { Analytics } from "@vercel/analytics/react";
 
 import Index from "./pages/Index";
 import About from "./pages/About";
@@ -32,6 +33,7 @@ const App = () => (
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
+          <Analytics />
         </PomodoroProvider>
       </TooltipProvider>
     </QueryClientProvider>
